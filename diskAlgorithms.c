fb30308674bae51ac365e911e658b942c904fc8f
Brent Rucker <brent.rucker@gmail.com>
2017-12-26 08:28:28 -0800

    Final Version of Disk Scheduling Algorithms
    More efficient algorithms. Reducing code by over 300 lines

--- diskAlgorithms.c
+++ diskAlgorithms.c
@@ -1,579 +1,263 @@
-/* 
-This program implements the following disck-scheduling algorithms:
+/*  This program implements the following disck-scheduling algorithms:
 FCFS, SSTF, SCAN, C-SCAN, LOOK, and C-LOOK
 The program services a disk of 5,000 cylinders numbered 0 to 4999.
 It generates a random series of 1000 cylinder requests and services 
 them according to each of he algorithms listed above. The program
 will be passed the initial position of the disk head (as a parameter
 on the command line) and report the total amount of head movement 
-required by each algorithm.
+required by each algorithm. 
+
+To run:
+gcc -o diskAlgorithms diskAlgorithms.c
+./file_name followed by number from 0-4999 for index (see below for example)
+./diskAlgorithms 423
 */
 
-/* 
-NOTE- New updated version is due to the lines which include: 
-  int* aTest = malloc(sizeof(int) * sizeof(ranArray)/ranArray[0]);  
-  these lines worked well for me and I had no compiling errors, however,
-  when Dr. Segal ran it, he had the following errors: 
-  Error in `./test': free(): invalid next size (fast): 0x0000000000819010 ***
-  This is because I should have malloced my array like so:
-   int* aTest = malloc(sizeof(int) * sizeof(ranArray));  
-   I did this and it still works. Thanks :)
-
-*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #define CYLINDERS 5000
 #define REQUESTS 1000
-#define ARRAYSIZE 11
 
+int start = 0;
 
-int arr[10][REQUESTS];
+int ran_array[REQUESTS];
+int test_array[REQUESTS];
 
-int determineAlgorithm(int * ranArray, int ranAlgorithm);
+int* sort_array() {
 
-int fcfs(int * ranArray);
-int sstf(int * ranArray);
-int scan(int * ranArray);
-int cscan(int * ranArray);
-int look(int * ranArray);
-int clook(int * ranArray);
+	int temp = 0, i = 0, j = 0;
 
-int* sort(int * ranArray);
+	for (i = 0; i < REQUESTS; ++i) {
 
-int start;
+       	for (j = i + 1; j < REQUESTS; ++j) {
 
-// THE GIVEN ARRAY IS FOR TESTING, 
-// IT IS USED TO MAKE SURE THE HEADVALUES ARE THE SAME AS THE HOMEWORK SOLUTIONS
-int ranArray[ARRAYSIZE]; // = {2150, 2069, 1212, 2296, 2800, 544, 1618, 356, 1523, 4965, 3681};
+            if (ran_array[i] > ran_array[j]) {
 
-
-int main (int argc, char *argv[]) {
-
-	start = atoi(argv[1]); 
-	printf("START Index: %d\n",start);
-
-	int i, j;
-	int requestCount = 0;
-		
-
-	// THIS IS FOR RANDOM SERIES OF 1000 CYLINDERS 
-	// COMMENT OUT FOR TESTING WITH HOMEWORK PROBLEMS
-	for(i = 0; i < ARRAYSIZE; i++) {
-		ranArray[i] = 0;
-	}
-	printf("RANDOM ARRAY\n");
-	for(i = 0; i < ARRAYSIZE; i++) {
-		int ran = rand() % 5000;
-		ranArray[i] = ran;
-		if(i == start) {
-			printf("START: %d\n",ranArray[i]);
-		}
-		else {
-			printf("%d\n",ranArray[i]);
-		}
-	}
-	// END  ----  THIS NEEDS TO BE UNCOMMENTED
-
-	
-
-	for(i = 0; i < 10; i++) {
-		for(j = 0; j < ARRAYSIZE; j++) {
-			arr[i][j] = 0;
-		}
-	}
-
-	while(requestCount < REQUESTS) {
-
-		int ranAlgorithm = rand() % 10;
-		//int ranAlgorithm = 3;
-		int headMovement = determineAlgorithm(ranArray, ranAlgorithm);
-		arr[ranAlgorithm][requestCount] = headMovement;
-		printf("Head Movement: %d\n",arr[ranAlgorithm][requestCount]);
-		requestCount++;
-		printf("COUNT: %d\n",requestCount);
-	}
-	printf("\nQUEUE Index and Head Value:\n0=FCFS\n1=SSTF\n2=SCAN\n3=C-SCAN\n4=LOOK\n5=C-LOOK\n");
-	for(i = 0; i < 6; i++) {
-		for(j = 0; j < REQUESTS; j++) {
-			if(arr[i][j] > 0) {
-				printf("Index: %d, Head Value: %d\n",i ,arr[i][j]);
-			}
-		}
-	}
-
-	
-	return 0;
-}
-
-int determineAlgorithm(int * ranArray, int ranAlgorithm) {
-	int headCount;
-	if(ranAlgorithm == 0) {
-		printf("\nFCFS\n");
-		headCount = fcfs(ranArray);
-		printf("FCFS %d\n",headCount);
-	}
-	else if(ranAlgorithm == 1) {
-		printf("\nSSTF\n");
-		headCount = sstf(ranArray);
-		printf("SSTF %d\n",headCount);
-	}
-	
-	else if(ranAlgorithm == 2) {
-		printf("\nSCAN\n");
-		headCount = scan(ranArray);
-		printf("SCAN %d\n",headCount);
-	}
-	else if(ranAlgorithm == 3) {
-		printf("\nC-SCAN\n");
-		headCount = cscan(ranArray);
-		printf("C-SCAN %d\n",headCount);
-	}
-	
-	else if(ranAlgorithm == 4) {
-		printf("\nLOOK\n");
-		headCount = look(ranArray);
-		printf("LOOK %d\n",headCount);
-	}
-	
-	else if(ranAlgorithm == 5) {
-		printf("\nC-LOOK\n");
-		headCount = clook(ranArray);
-		printf("C-LOOK %d\n",headCount);
-	}
-	
-	else {
-		return -1;
-	}
-
-	return headCount;
-}
-
-int fcfs(int * ranArray) {
-
-	int i;
-	int head_movement;
-    for(i=start; i < ARRAYSIZE; i++) {
-    	if(i+1 < ARRAYSIZE) {
-    		if(ranArray[i+1] > ranArray[i]) {
-	    		int num = ranArray[i+1] - ranArray[i];
-	    		//printf("num: %d\n",num);
-	    		head_movement = head_movement+ num;
-	    		//printf("Head1: %d\n",head_movement);
-    		}
-    		else {
-    			int num = ranArray[i] - ranArray[i+1];
-	    		//printf("num: %d\n",num);
-	    		head_movement = head_movement+ num;
-	    		//printf("Head1: %d\n",head_movement);
-    		}
-    	}
-    	else {
-    		break;
-    	}
+                temp =  ran_array[i];
+                ran_array[i] = 	ran_array[j];
+                ran_array[j] = temp;
+            }
+        }
     }
-    for(i=0; i < start; i++) {
-    	if(i == 0) {
-    		if(ranArray[9] > ranArray[0]) {
-	    		int num2 = ranArray[ARRAYSIZE-1] - ranArray[0];
-	    		//printf("num2: %d\n",num2);
-	    		head_movement = head_movement + num2;
-	    		//printf("Head2: %d\n",head_movement);
-    		}
-    		else {
-    			int num2 = ranArray[0] - ranArray[ARRAYSIZE-1];
-	    		//printf("num2: %d\n",num2);
-	    		head_movement = head_movement + num2;
-	    		//printf("Head2: %d\n",head_movement);
-    		}
-    	}
-    	else {
-    		if(ranArray[i] > ranArray[i-1]) {
-    			int num3 = ranArray[i] - ranArray[i-1];
-    			//printf("num3: %d\n",num3);
-    			head_movement = head_movement+ num3;
-    			//printf("Head3: %d\n",head_movement);
-    		}
-    		else {
-    			int num3 = ranArray[i-1] - ranArray[i];
-    			//printf("num3: %d\n",num3);
-    			head_movement = head_movement+ num3;
-    			//printf("Head3: %d\n",head_movement);
-    		}
-    	}
+
+    return ran_array;
+}
+
+
+/* First-Come-First-Serve (fcfs) starts from the index after the starting
+index and continually adds the headmovement from the starting index in 
+order recieved. If at end of array, start from index zero and continually 
+add until starting index */
+int fcfs(int *ran_array) {
+
+	int i = 0, head_movement = 0, this_start = ran_array[start];
+
+    for(i = start; i < REQUESTS; i++) {
+
+    	head_movement += abs(ran_array[i] - this_start);
+    }
+
+    for(i = 0; i < start; i++) {
+
+    	head_movement += abs(this_start - ran_array[i]);
     }
         
     return head_movement;
 }
 
+/* Shortest-Seek-Time-First (SSTF) takes the current head position, and
+adds the position closest to the current head. This new position now becomes
+the head, then this system repeats. 
+First we sort the array. Then We have counters for above and below start 
+index that we decrement if used. Once these equal to REQUEST-2 (excluding 
+start index) we exit. */
+int sstf(int * ran_array) {
 
-int sstf(int * ranArray) {
+	ran_array = sort_array();
 
-	int shortest = 5001;
-	int j;
-	int hey = ARRAYSIZE;
-	int newStart = ranArray[start];
-	int newHead = 0;
-	int saveCurrent;
-	int arrTest[ARRAYSIZE];
-	int saveLocation;
-	int i = 0;
+	int small_i = start-1, large_i = start+1;
+	int small_diff = 0, large_diff = 0;
+	int head_movement = 0, total = REQUESTS-2, new_head = start, head_value = ran_array[start];
+	
+	while(total >= 0) {
 
-	for(i = 0; i < ARRAYSIZE; i++) {
-		arrTest[i] = 0;
+		small_diff = abs(ran_array[new_head] - ran_array[small_i]);
+		large_diff = abs(ran_array[large_i] - ran_array[new_head]);
+
+		if(small_diff < large_diff) {
+
+			head_movement += small_diff;
+			new_head = small_i;
+			small_i--;
+			
+		} else {
+
+			head_movement += large_diff;
+			new_head = large_i;
+			large_i++;
+		}
+
+		total--;
+
 	}
-	arrTest[start]++;
 
-	while(hey > 0) {
+	return head_movement;
 
-		int distance;
-		
-		for(j = 0; j< ARRAYSIZE; j++) {
-
-			int current = ranArray[j];
-			int used = arrTest[j];
-
-			if(used < 1 && newStart > current) {
-				distance = newStart - current;
-			}
-			else if(used < 1 && newStart < current) {
-				distance = current - newStart;
-			}
-			if(used < 1 && distance < shortest) {
-				shortest = distance;
-				saveCurrent = current;
-				saveLocation = j;
-			}
-		}
-
-		distance = 0;
-		arrTest[saveLocation]++;
-		int num = arrTest[saveLocation];
-		int difference = 0;
-
-		if(num < 2 && saveCurrent < newStart) {
-			difference = newStart - saveCurrent;
-			newHead += difference;
-		}
-		else if(num < 2 && saveCurrent > newStart) {
-			difference = saveCurrent - newStart;
-			newHead += difference;
-		}
-
-		printf("NEW HEAD FINAL: %d\n",newHead);
-		newStart = saveCurrent;
-		saveCurrent = 0;
-		shortest = 5001;
-		--hey;
-	}
-	return newHead;
 }
 
+/* SCAN - array is already sorted from sstf. SCAN starts from one left of start, 
+and continually goes down to zero (if included in randome array or not). Then 
+starts at one higher than start and continually goes up to highest value (not 5000) */
 int scan(int * ranArray) {
 
-	int i = 0;
-	int j = 0;
-	int* aTest = malloc(sizeof(int) * sizeof(ranArray)); 
+	int i = 0, curr_val = 0, sav_val = ran_array[start], difference = 0;
+	int head_movement = 0, curr_i = 0;
 
-	for(i = 0; i < ARRAYSIZE; i++) {
-		aTest[i] = 0;
-	}
-	for(i = 0; i < ARRAYSIZE; i++) {
-		aTest[i] = ranArray[i];
-	}
+	for(i = start-1; i >= 0; --i) {
 
-	int origStart = ranArray[start];
-	int arrFinal[ARRAYSIZE];
+		curr_val = ran_array[i];
+		difference = abs(sav_val - curr_val);
+		head_movement += difference;
+		sav_val = curr_val;
 
-	ranArray = sort(aTest);
-
-	//for(i = 0; i < ARRAYSIZE; i++) {
-	//	printf("RETURNED %d\n",ranArray[i]);
-	//}
-
-	int posit;
-	int saveNum;
-	int headTotal = 0;
-
-	for(i = 0; i < ARRAYSIZE; i++) {
-		arrFinal[i] = 0;
-		int num = ranArray[i];
-		if(num == origStart) {
-			posit = i;
-			saveNum = num;
-			printf("START: %d, Index: %d\n",saveNum, posit);
-		}
 	}
 
-	int thisOne;
-	//if(posit == 0) {
-	//	posit = ARRAYSIZE-1;
-	//}
-	for(i = posit-1; i >= 0; --i) {
-		thisOne = ranArray[i];
-		//printf("This One: %d\n",thisOne);
-		int difference = saveNum - thisOne;
-		//printf("S Diff1 %d\n",difference);
-		arrFinal[j] = difference;
-		saveNum = thisOne;
-		headTotal += difference;
-		printf("Total: %d\n",headTotal);
-		j++;
-	}
-	int thisDif = thisOne - 0;
-	//printf("End diff: %d\n",thisDif);
-	headTotal += thisDif - 0;
-	printf("Total End: %d\n",headTotal);
-	saveNum = 0;
+	/* used to subtract value from zero, or just add same value */
+	head_movement += sav_val;
+	sav_val = 0;
 
-	for(i = posit+1; i < ARRAYSIZE; i++) {
-		thisOne = ranArray[i];
-		int difference = thisOne - saveNum;
-		//printf("L Diff2 %d\n",difference);
-		arrFinal[j] = difference;
-		headTotal += difference;
-		printf("Total: %d\n",headTotal);
-		saveNum = thisOne;
-		j++;
+	for(i = start+1; i < REQUESTS; i++) {
+
+		curr_val = ran_array[i];
+		difference = abs(curr_val - sav_val);
+		head_movement += difference;
+		sav_val = curr_val;
+
 	}
 
-	free(aTest);
+	return head_movement;
 
-	return headTotal;
 }
 
-
+/* Circular Scan (C-SCAN) - start at start index, increase to upper boundary 
+(even if no value at boundary), save boundary value, go to start boundary 
+(zero value) increase till last value before start value */
 int cscan(int * ranArray) {
 
-	int i = 0;
-	int j = 0;
-	int* aTest = malloc(sizeof(int) * sizeof(ranArray));
+	int i = 0, curr_val = 0, sav_val = ran_array[start], difference = 0;
+	int head_movement = 0, curr_i = 0, upper_bound = 4999;
 
-	for(i = 0; i < ARRAYSIZE; i++) {
-		aTest[i] = 0;
-	}
-	for(i = 0; i < ARRAYSIZE; i++) {
-		aTest[i] = ranArray[i];
-	}
+	for(i = start+1; i < REQUESTS; i++) {
 
-	int origStart = ranArray[start];
-	int arrFinal[ARRAYSIZE];
+		curr_val = ran_array[i];
+		difference = abs(sav_val - curr_val);
+		head_movement += difference;
+		sav_val = curr_val;
 
-	ranArray = sort(aTest);
-
-	//for(i = 0; i < ARRAYSIZE; i++) {
-	//	printf("RETURNED %d\n",ranArray[i]);
-	//}
-
-	int posit;
-	int saveNum;
-	int headTotal = 0;
-
-	for(i = 0; i < ARRAYSIZE; i++) {
-		arrFinal[i] = 0;
-		int num = ranArray[i];
-		if(num == origStart) {
-			posit = i;
-			saveNum = num;
-			printf("START: %d, Index: %d\n",saveNum, posit);
-		}
 	}
 
-	int thisOne;
+	/* add last val - upper bound, go to and add zero bounday (4999)*/
+	head_movement += upper_bound - sav_val;
+	sav_val = 0;
+	head_movement += 4999;
 
-	for(i = posit+1; i < ARRAYSIZE; i++) {
-		thisOne = ranArray[i];
-		int difference = thisOne - saveNum;
-		//printf("L Diff2 %d\n",difference);
-		arrFinal[j] = difference;
-		headTotal += difference;
-		printf("Total: %d\n",headTotal);
-		saveNum = thisOne;
-		j++;
+	for(i = 0; i < start; i++) {
+
+		curr_val = ran_array[i];
+		difference = abs(curr_val - sav_val);
+		head_movement += difference;
+		sav_val = curr_val;
+
 	}
 
-	int thisDif = 4999 - saveNum;
-	//printf("End diff: %d\n",thisDif);
-	headTotal += thisDif;
-	printf("Total End1: %d\n",headTotal);
-	saveNum = 0;
-	headTotal += 4999;
-	printf("Total End2: %d\n",headTotal);
-
-	for(i = 0; i < posit; i++) {
-		thisOne = ranArray[i];
-		//printf("This One: %d\n",thisOne);
-		int difference;
-		difference = thisOne - saveNum;
-		//printf("S Diff1 %d\n",difference);
-		arrFinal[j] = difference;
-		saveNum = thisOne;
-		headTotal += difference;
-		printf("Total: %d\n",headTotal);
-		j++;
-	}
-	
-	free(aTest);
-
-	return headTotal;
+	return head_movement;
 }
 
-
-
+/* Look - start from value above start, increase to highest value. 
+Then goes to value below start value and decreases until smallest value */
 int look(int* ranArray) {
 
-	int i = 0;
-	int j = 0;
-	int* aTest = malloc(sizeof(int) * sizeof(ranArray));
+	int i = 0, curr_val = 0, sav_val = ran_array[start], difference = 0;
+	int head_movement = 0, curr_i = 0;
 
-	for(i = 0; i < ARRAYSIZE; i++) {
-		aTest[i] = 0;
-	}
-	for(i = 0; i < ARRAYSIZE; i++) {
-		aTest[i] = ranArray[i];
-	}
+	for(i = start+1; i < REQUESTS; i++) {
 
-	int origStart = ranArray[start];
-	int arrFinal[ARRAYSIZE];
+		curr_val = ran_array[i];
+		difference = abs(sav_val - curr_val);
+		head_movement += difference;
+		sav_val = curr_val;
 
-	ranArray = sort(aTest);
-
-	//for(i = 0; i < ARRAYSIZE; i++) {
-	//	printf("RETURNED %d\n",ranArray[i]);
-	//}
-
-	int posit;
-	int saveNum;
-	int headTotal = 0;
-
-	for(i = 0; i < ARRAYSIZE; i++) {
-		arrFinal[i] = 0;
-		int num = ranArray[i];
-		if(num == origStart) {
-			posit = i;
-			saveNum = num;
-		}
 	}
 
-	int thisOne;
+	for(i = start-1; i >= 0; --i) {
 
-	for(i = posit+1; i < ARRAYSIZE; i++) {
-		thisOne = ranArray[i];
-		int difference = thisOne - saveNum;
-		//printf("L Diff2 %d\n",difference);
-		arrFinal[j] = difference;
-		headTotal += difference;
-		printf("Total: %d\n",headTotal);
-		saveNum = thisOne;
-		j++;
+		curr_val = ran_array[i];
+		difference = abs(curr_val - sav_val);
+		head_movement += difference;
+		sav_val = curr_val;
+
 	}
 
-	for(i = posit-1; i >= 0; --i) {
-		thisOne = ranArray[i];
-		int difference = saveNum - thisOne;
-		//printf("L Diff1 %d\n",difference);
-		arrFinal[j] = difference;
-		saveNum = thisOne;
-		headTotal += difference;
-		printf("Total: %d\n",headTotal);
-		j++;
-	}
-
-	free(aTest);
-
-	return headTotal;
+	return head_movement;
 }
 
+/* C-Look - Starts from value after start value, goes to highest value, 
+then goes to smallest value and increases until value before start value */
 int clook(int* ranArray) {
 
-	int i = 0;
-	int j = 0;
-	int* aTest = malloc(sizeof(int) * sizeof(ranArray));
+	int i = 0, curr_val = 0, sav_val = ran_array[start], difference = 0;
+	int head_movement = 0, curr_i = 0;
 
-	for(i = 0; i < ARRAYSIZE; i++) {
-		aTest[i] = 0;
-	}
-	for(i = 0; i < ARRAYSIZE; i++) {
-		aTest[i] = ranArray[i];
-		//printf("TEST: %d\n",aTest[i]);
-	}
+	for(i = start+1; i < REQUESTS; i++) {
 
-	int arrFinal[ARRAYSIZE];
-	int origStart = ranArray[start];
+		curr_val = ran_array[i];
+		difference = abs(sav_val - curr_val);
+		head_movement += difference;
+		sav_val = curr_val;
 
-	ranArray = sort(aTest);
-
-	//for(i = 0; i < 10; i++) {
-	//	printf("RETURNED %d\n",ranArray[i]);
-	//}
-
-	int posit;
-	int saveNum;
-	int headTotal = 0;
-
-	for(i = 0; i < ARRAYSIZE; i++) {
-		arrFinal[i] = 0;
-		int num = ranArray[i];
-		if(num == origStart) {
-			posit = i;
-			saveNum = num;
-		}
 	}
 
-	int thisOne;
-	for(i = posit+1; i < ARRAYSIZE; i++) {
-		thisOne = ranArray[i];
-		int difference = thisOne - saveNum;
-		//printf("Diff1 %d\n",difference);
-		arrFinal[j] = difference;
-		headTotal += difference;
-		printf("Total: %d\n",headTotal);
-		saveNum = thisOne;
-		j++;
-	}
+	for(i = 0; i < start; i++) {
 
-	for(i = 0; i < posit; i++) {
-		thisOne = ranArray[i];
-		int difference;
-		if(i == 0) {
-			difference = saveNum - thisOne;
-			//printf("Diff2 %d\n",difference);
-		}
-		else {
-			difference = thisOne - saveNum;
-			//printf("Diff3 %d\n",difference);
-		}
-		arrFinal[j] = difference;
-		saveNum = thisOne;
-		headTotal += difference;
-		printf("Total: %d\n",headTotal);
-		j++;
+		curr_val = ran_array[i];
+		difference = abs(curr_val - sav_val);
+		head_movement += difference;
+		sav_val = curr_val;
+
 	}	
 
-	free(aTest);
-
-	return headTotal;
+	return head_movement;
 }
 
-int* sort(int * aTest) {
 
-	int a;
+int main (int argc, char *argv[]) {
+
+
 	int i = 0;
-	int j = 0;
-	for (i = 0; i < ARRAYSIZE; ++i) {
-       	for (j = i + 1; j < ARRAYSIZE; ++j) {
-            if (aTest[i] > aTest[j]) {
-                a =  aTest[i];
-                aTest[i] = 	aTest[j];
-                aTest[j] = a;
-            }
-        }
-    }
+	start = atoi(argv[1]);
 
-	return aTest;
+	if(argc != 2) {
+
+		printf("Please compile program with starting index from 0-4999. Ex. ./diskAlgorithms 423\n");
+		exit(-1);
+	}
+
+	for(i = 0; i < REQUESTS; i++) {
+
+		ran_array[i] = rand() % 5000;
+	}
+
+	printf("\nStart index: %d, start value: %d\n\n", start, ran_array[start]);
+
+	printf("FCFS head movements: %d\n", fcfs(ran_array));
+	printf("SSTF head movements: %d\n", sstf(ran_array));
+	printf("SCAN head movements: %d\n", scan(ran_array));
+	printf("CSCAN head movements: %d\n", cscan(ran_array));
+	printf("LOOK head movements: %d\n", look(ran_array));
+	printf("C-LOOK head movements: %d\n\n", clook(ran_array));
+
+	return 0;
 }
-
-
-
 
